4893b2a5b15030ef9ac7daef5e04ff52ccf41808
Dan <saratoubi76@gmail.com>
2020-02-20 17:43:50 -0500

    Added a couple shy sprites to play around with, plus a base DamageSystem Class, and a test player class

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : DamageSystem
+{
+    public class PlayerSystem : DamageSystem
+    {
+        public PlayerSystem()
+        {
+
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
